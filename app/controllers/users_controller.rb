21e8e80a88d61d8f56aad27a70126dc2aaa3f351
Anastasia Fountis <afountis@gmail.com>
2015-12-19 15:08:32 +1100

    Add user creation form and user list page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(first_name: params[:user][:first_name], last_name: params[:user][:last_name], email: params[:user][:email])
+    redirect_to users_path 
+  end
 end

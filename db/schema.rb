c00db29dfa199191c40afeb70c68b39e486b78ea
Anastasia Fountis <afountis@gmail.com>
2016-02-24 22:22:32 +1100

    Add relationship between users and deparments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219050556) do
+ActiveRecord::Schema.define(version: 20160224110947) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20151219050556) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "department_id"
   end
 
 end

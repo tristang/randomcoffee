81e75add8d8b955c20dd8a2a7f278fd3bc2ea1ff
Anastasia Fountis <afountis@gmail.com>
2016-03-06 19:02:17 +1100

    Sort users alphabetically

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,6 @@ class AdminController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "temporarypassword"
 
   def users
-    @user_list = User.all
+    @user_list = User.all.order(first_name: :asc)
   end
-
-
 end

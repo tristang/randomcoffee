933a22924e066fd3308e0fa11528732af93f2c5b
Anastasia Fountis <afountis@gmail.com>
2016-03-06 19:27:30 +1100

    Use environment variable for password

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
 
-  http_basic_authenticate_with name: "admin", password: "temporarypassword"
+  http_basic_authenticate_with name: "admin", password: ENV['ADMIN_PASSWORD']
 
   def users
     @user_list = User.all.order(first_name: :asc)

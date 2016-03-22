aca81a15d893bf74658ff5152b1cc588b2388b7b
Anastasia Fountis <afountis@gmail.com>
2016-03-22 23:04:10 +1100

    Add admin pages to update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,23 @@ class Admin::UsersController < AdminController
   def index
     @user_list = User.all.order(first_name: :asc)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :department_id)
+  end
 end

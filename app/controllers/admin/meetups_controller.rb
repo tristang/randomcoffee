d97387efa7fe1f1f26b59f551171bcac7895f6bf
Anastasia Fountis <afountis@gmail.com>
2016-03-31 21:59:28 +1100

    Allow admins to create new meetups

--- app/controllers/admin/meetups_controller.rb
+++ app/controllers/admin/meetups_controller.rb
@@ -2,4 +2,14 @@ class Admin::MeetupsController < AdminController
   def index
   end
 
+  def create
+    meetup = Meetup.new
+    meetup.save
+    redirect_to(admin_meetup_path(meetup))
+  end
+
+  def show
+    @meetup = Meetup.find(params[:id])
+  end
+
 end

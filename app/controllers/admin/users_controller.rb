59da478921bd43912faa1169df8f1cad3fcb2e4b
Anastasia Fountis <afountis@gmail.com>
2016-04-07 09:25:34 +1000

    Allow users to be deleted

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,9 +10,22 @@ class Admin::UsersController < AdminController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to admin_users_path
+      redirect_to admin_users_path, notice: "Changes saved successfully."
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if Pairing.where(user_1: @user).any? || Pairing.where(user_2: @user).any?
+      return redirect_to :back, flash: { error: "Can't delete that user because they've already been paired!" }
+    end
+
+    if @user.destroy
+      redirect_to admin_users_path, notice: "Deleted #{@user.full_name} successfully."
+    else
+      redirect_to :back
     end
   end
 

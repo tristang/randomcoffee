1677b667a10dcdc79d96929d5b946d3ec33f03b4
Tristan Gamilis <tristan@2suggestions.com.au>
2016-04-07 22:35:41 +1000

    Add test mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,8 @@ class UserMailer < ApplicationMailer
     @partner = partner
     mail(to: recipient.email, subject: "Have a coffee with #{partner.full_name} from #{partner.department.name}")
   end
+
+  def test_email(email)
+    mail(to: email, subject: "This was from Mailgbun")
+  end
 end

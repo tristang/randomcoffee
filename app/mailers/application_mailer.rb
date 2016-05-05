2b732c5ecbc3cedc7a40c804c4c2ac7eb6e2c2d6
Tristan Gamilis <tristan@2suggestions.com.au>
2016-05-06 09:56:24 +1000

    Make mailers send from Mailgun sandbox domain with reply to set

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Kate Brewster <kate.brewster@boroondara.vic.gov.au>"
+  default from: "Innovation and Business Transformation <postmaster@app3c3d8ad576534e8d8f6d4f45fb220a66.mailgun.org>"
+  default reply_to: "Innovation and Business Transformation <boroondara.innovation@boroondara.vic.gov.au>"
   layout 'mailer'
 end

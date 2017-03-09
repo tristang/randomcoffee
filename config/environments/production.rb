486c0fff202691ae399c538958e01be2923eae26
Tristan Gamilis <tristan@2suggestions.com.au>
2017-03-09 23:32:57 +1100

    Update production domain to randomacts.coffee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'random-acts-of-coffee.herokuapp.com',
+    :domain         => 'randomacts.coffee',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

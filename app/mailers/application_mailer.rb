59c40c95dd9612e6dafd4d36006cbb961f2b21df
Tristan Gamilis <tristan@2suggestions.com.au>
2017-03-09 23:34:49 +1100

    Set default mail sender to postmaster@mg.randomacts.coffee

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Innovation and Business Transformation <postmaster@app3c3d8ad576534e8d8f6d4f45fb220a66.mailgun.org>"
+  default from: "Innovation and Business Transformation <postmaster@mg.randomacts.coffee>"
   default reply_to: "Innovation and Business Transformation <boroondara.innovation@boroondara.vic.gov.au>"
   layout 'mailer'
 end

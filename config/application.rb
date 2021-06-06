9d44d83a5d418f20e305bc83da097b2e9b8d0725
Tristan Gamilis <tristan@usabilityhub.com>
2021-06-06 14:53:22 +1000

    Upgrade hackily to rails 5.0.1

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Randomcoffee
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile = ["manifest.js"]
   end
 end

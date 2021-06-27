ec93dffaafebbfbbc25ee2202c3b5b19a7d1638d
Tristan Gamilis <tristan@usabilityhub.com>
2021-06-27 14:42:39 +1000

    Update pairing email copy

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "MYOB Random acts of coffee <postmaster@mg.randomacts.coffee>"
+  default from: "MYOB random acts of coffee <postmaster@mg.randomacts.coffee>"
   default reply_to: "Tas Fountis <tas@myob.com>"
   layout 'mailer'
 end
